f71a359f204e7f29b69d229e4aa96427170c9224
Ryan Resella <ryanresella@gmail.com>
2013-06-01 11:51:24 -0400

    Cache the homepage call to the we the people api

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.12'
 gem 'jquery-rails'
 gem 'we_the_people', '~> 0.0.6'
+gem 'dalli'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     thirty_days = 30*24*60*60
-    active_petitions = WeThePeople::Resources::Petition.all(nil, :createdAfter => thirty_days)
+    active_petitions = Rails.cache.fetch(thirty_days) { WeThePeople::Resources::Petition.all(nil, :createdAfter => thirty_days) }
     pending_petitions = active_petitions.select do |petition|
       (petition.status == "open") && (petition.signatures_needed > 0)
     end
