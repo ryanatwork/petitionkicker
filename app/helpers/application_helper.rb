5a3168300bfe4acf12bbe6ca05fca06834243a48
Ryan Resella <ryanresella@gmail.com>
2013-06-01 15:11:45 -0400

    Change the progress bar based on the colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def progress_percentage(percentage)
+    if percentage.to_i < 10
+      "progress-danger"
+    elsif percentage.to_i > 10 && percentage.to_i < 40
+      "progress-warning"
+    else
+      "progress-info"
+    end
+  end
 end
